6619daa786ccf04b43d11b499f682a2222ee7aed
Pedro Veras <veraspedro1608@gmail.com>
2023-09-07 16:12:27 -0300

    feat: :sparkles: install font inter in application

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
 import { StatusBar } from 'react-native';
 
 import { Routes } from '@routes/index';
+import { Loading } from '@components/Loading';
+
+import {
+  useFonts,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+
   return (
     <>
       <StatusBar
@@ -10,7 +24,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Routes />
+      {fontsLoaded ? <Routes /> : <Loading />}
     </>
   );
 }
